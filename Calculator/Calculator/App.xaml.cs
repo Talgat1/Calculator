200d98e2067c5bf0740cef3944f404e80532fb63
arullintalgat36@gmail.com <arullintalgat36@gmail.com>
2022-03-25 09:23:53 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Calculator/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    public partial class App : Application
+    {
+        Calc calc;
+        public App()
+        {
+            InitializeComponent();
+            calc = new Calc();
+            calc.RestoreState(Current.Properties);
+            MainPage = new MainPage(calc);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
